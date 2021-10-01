a42d61a657c72b3067d078d97480dd9c43ec8ef0
Alexander Chirkin <chalex2000@yandex.ru>
2021-10-02 00:53:34 +0300

    Добавил первый endpoint

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 from aiohttp import web
 
+routes = web.RouteTableDef()
+
+
+@routes.get('/abc')
+async def abc_handler(request: web.Request) -> web.Response:
+    print('handle abc')
+    return web.Response(text='abc response')
+
 app = web.Application()
-web.run_app(app)
+app.add_routes(routes)
+web.run_app(app, port=8080)
+
+
